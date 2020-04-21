e741a95638df786824ea9d4f4746db54ae4f80ee
hoanglong0512 <62041670+hoanglong0512@users.noreply.github.com>
2020-04-21 17:00:23 +0700

    làm

--- truu_tuong.php
+++ truu_tuong.php
@@ -1,3 +1,58 @@
 <?php
-echo "hello";
+
+// VD về tính đóng trừu tượng
+ 
+// VD 1
+
+class SmartPhone{
+	var $thuong_hieu;
+	var $Ten;
+	var $mau_sac;
+	var $man_hinh;
+	var $Bo_nho;
+	var $dung_luong_pin;
+	var $he_dieu_hanh;
+
+	function Nghe(){}
+	function Goi(){}
+	function NhanTin(){}
+	function ChupAnh(){}
+	function QuayClip(){}
+	function NgheNhac(){}
+	function LuotWeb(){}
+	function XemPhim(){}
+	function ChoiGame(){}
+
+}
+
+
+// VD 2
+ class XeMay{
+    var $thuong_hieu;
+    var $Ten;
+    Var $mau_sac;
+    var $Trong_luong;
+    var $ChieuDai;
+    var $ChieuRong;
+    var $ChieuCao;
+    var $dung_tich_binh_xang;
+    var $dung_tich_xi_lanh;
+
+    function MoKhoa(){}
+    function NoMay(){}
+    function VaoSo(){}
+    function TangGa(){}
+    function DiChuyen(){}
+    function BamCoi(){}
+    function BatDen(){}
+    function BatXiNhan(){}
+    function DoXang(){}
+    function SuaXe(){}
+
+
+ }
+
+
+
+
 ?>
